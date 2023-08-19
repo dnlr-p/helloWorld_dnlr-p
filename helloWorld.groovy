98b352a9996f4515ebee51e79c5b032c73509cb9
dnlr-p <61924695+dnlr-p@users.noreply.github.com>
2023-08-19 19:31:15 -0400

    Added a print statement
    Tutorial

--- helloWorld.groovy
+++ helloWorld.groovy
@@ -1,3 +1,6 @@
 import com.neuronrobotics.bowlerstudio.BowlerKernel
+//Tutorial println function
+println "This is terminal console output."
+
 // Have BowlerStudio speak messages
 BowlerKernel.speak("Hello!")
